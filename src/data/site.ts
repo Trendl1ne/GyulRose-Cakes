6dbd011cec0d04aee7df822ba816c24e3c94f191
Muro Testing <napravigolesno@gmail.com>
2026-07-13 15:38:18 +0100

    Integrate proven homepage with search site

--- src/data/site.ts
+++ src/data/site.ts
@@ -4,6 +4,7 @@ export const site = {
   phoneDisplay: "+44 7455 802400",
   phoneInternational: "447455802400",
   email: "gyulrose84@gmail.com",
+  orderWebhook: "https://n8n-bopk.onrender.com/webhook/e20c256e-b7b7-4a2f-bdba-692b81479d0c",
   instagram: "https://instagram.com/gyulok84",
   instagramHandle: "@gyulok84",
   tiktok: "https://tiktok.com/@gyulrose",
